068a1e159d9bc96f4087ceebef121fb790888973
Raj1908 <rajat19081996kulkarni@gmail.com>
2024-07-21 02:00:02 +0530

    ProjectManagementSystem

---
+++ ProjectManagementSystem/src/test/java/com/management/projectmanagement/ProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.projectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
