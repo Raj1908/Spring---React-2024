542ce446f1cb24d1ec733f03a9d1e8438286d34f
Raj1908 <rajat19081996kulkarni@gmail.com>
2024-07-21 02:24:23 +0530

    db props added in yml

--- ProjectManagementSystem/src/main/java/com/management/ProjectManagementSystemApplication.java
+++ ProjectManagementSystem/src/main/java/com/management/ProjectManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package com.management.ProjectManagementSystem;
+package com.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
